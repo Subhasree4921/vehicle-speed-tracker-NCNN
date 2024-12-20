6a655fd8d66eb976da40c68599429983747bca84
DanielaUwU <98227714+Subhasree4921@users.noreply.github.com>
2024-12-20 15:22:21 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -3,26 +3,24 @@ from tracker import ObjectCounter  # Importing ObjectCounter from tracker.py
 
 # Define the mouse callback function
 def RGB(event, x, y, flags, param):
-    if event == cv2.EVENT_MOUSEMOVE:  # Check for mouse movement
+    if event == cv2.EVENT_MOUSEMOVE:  
         point = [x, y]
         print(f"Mouse moved to: {point}")
 
-# Open the video file
-cap = cv2.VideoCapture('tf.mp4')
+
+cap = cv2.VideoCapture(0)
 
 # Define region points for counting
 region_points = [(0, 112), (224, 112)]
-# Initialize the object counter
-counter = ObjectCounter(
-    region=region_points,  # Pass region points
-    model="yolo11n_ncnn_model",  # Model for object counting
-    classes=[2,5,7],  # Detect only cars,truck,motorcycles
-    show_in=True,  # Display in counts
-    show_out=True,  # Display out counts
-    line_width=2,  # Adjust line width for display
-)
 
-# Create a named window and set the mouse callback
+counter = ObjectCounter(
+    region=region_points,  
+    model="yolo11n_ncnn_model",  
+    classes=[2,5,7], 
+    show_in=True,  
+    show_out=True,  
+    line_width=2,  
+)
 
 
 count = 0
@@ -32,11 +30,9 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
-        # If video ends, reset to the beginning
-#        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-#        continue
+        
     count += 1
-    if count % 5 != 0:  # Skip odd frames
+    if count % 5 != 0:  
         continue
 
     frame = cv2.resize(frame, (224, 224), interpolation=cv2.INTER_NEAREST)
@@ -49,6 +45,6 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord("q"):  # Press 'q' to quit
         break
 
-# Release the video capture object and close the display window
+
 cap.release()
 cv2.destroyAllWindows()
